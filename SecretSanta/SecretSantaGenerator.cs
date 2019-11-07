b6689ff5b50002cf3998ca9dac5e8aae265a2e6a
Adria Lopez <adria.lopez@appsnfriends.com>
2019-11-07 17:25:40 +0100

    Created interface, refactored Generate methods and added excludeMutualPairing feature
    [#] Unified all generate logic in GenerateResults method
    [#] Added parameters for excludeMutualPairing
    [#] Added method to assert mutual pairings
    [#] Added interface

--- SecretSanta/SecretSantaGenerator.cs
+++ SecretSanta/SecretSantaGenerator.cs
@@ -5,53 +5,66 @@ using System.Linq;
 
 namespace SecretSanta
 {
-    public class SecretSantaGenerator
+    public class SecretSantaGenerator : ISecretSantaGenerator
     {
-        public IDictionary<T, T> Generate<T>(IList<T> participants)
+        public IDictionary<T, T> Generate<T>(IList<T> participants, bool excludeMutualPairing = false)
         {
-            return this.Generate(participants, new Dictionary<T, T>());
+            return this.Generate(participants, new Dictionary<T, T>(), excludeMutualPairing);
         }
 
-        public IDictionary<T, T> Generate<T>(IList<T> participants, IDictionary<T, T> bannedPairings)
+        public IDictionary<T, T> Generate<T>(IList<T> participants, IDictionary<T, T> bannedPairings, bool excludeMutualPairing = false)
         {
-            var to = participants.GetShuffle();
-
-            foreach (var from in participants.GetShuffle().GetPermutations())
+            var results = this.GenerateResults(participants, bannedPairings, true, excludeMutualPairing);
+            try
             {
-                var result = to.ZipToKV(from).ToList();
-
-                if (this.PairingIsValid(bannedPairings, result))
-                {
-                    return result.ToDictionary();
-                }
+                return results.Single();
             }
-
-            throw new ApplicationException("No valid santa list can be generated");
+            catch (Exception ex)
+            {
+                throw new ApplicationException("No valid santa list can be generated");
+            }
         }
 
-        private bool PairingIsValid<T>(IDictionary<T, T> bannedPairings, IEnumerable<KeyValuePair<T, T>> result)
+        public IEnumerable<IDictionary<T, T>> GenerateAll<T>(IList<T> participants, bool excludeMutualPairing = false)
         {
-            return result.All(r => !r.Key.Equals(r.Value) && !bannedPairings.Contains(r));
+            return this.GenerateAll(participants, new Dictionary<T, T>(), excludeMutualPairing);
         }
 
-        public IEnumerable<IDictionary<T, T>> GenerateAll<T>(IList<T> participants)
+        public IEnumerable<IDictionary<T, T>> GenerateAll<T>(IList<T> participants, IDictionary<T, T> bannedPairings, bool excludeMutualPairing = false)
         {
-            return this.GenerateAll(participants, new Dictionary<T, T>());
+            return this.GenerateResults(participants, bannedPairings, false, excludeMutualPairing);
         }
 
-        public IEnumerable<IDictionary<T, T>> GenerateAll<T>(IList<T> participants, IDictionary<T, T> bannedPairings)
+        private IEnumerable<IDictionary<T, T>> GenerateResults<T>(IList<T> participants, IDictionary<T, T> bannedPairings,
+            bool getJustOneResult, bool excludeMutualPairing = false)
         {
             var to = participants.GetShuffle();
 
             foreach (var from in participants.GetShuffle().GetPermutations())
             {
-                var result = to.ZipToKV(from).ToList();
+                var result = to.MergeToKeyValuePair(from).ToList();
 
-                if (this.PairingIsValid(bannedPairings, result))
+                if (this.PairingIsValid(bannedPairings, result, excludeMutualPairing))
                 {
                     yield return result.ToDictionary();
+                    if (getJustOneResult)
+                    {
+                        yield break;
+                    }
                 }
             }
+        }
+
+        private bool PairingIsValid<T>(IDictionary<T, T> bannedPairings, IEnumerable<KeyValuePair<T, T>> result, bool excludeMutualPairing)
+        {
+            return result.All(r => !r.Key.Equals(r.Value)
+                                   && !bannedPairings.Contains(r)
+                                   && !(excludeMutualPairing && this.IsMutualPairing(r)));
+        }
+
+        private bool IsMutualPairing<T>(KeyValuePair<T, T> r)
+        {
+            return r.Equals(new KeyValuePair<T, T>(r.Value, r.Key));
         }
     }
 }
