99623e7f46fd981871420c1ecb25667941872524
Adria Lopez <adria.lopez@appsnfriends.com>
2019-11-15 12:10:25 +0100

    Added WebAPI to execute the Secret Santa

---
+++ SecretSantaWebAPI/Controllers/SecretSantaController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SecretSanta;
+
+namespace SecretSantaWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SecretSantaController : ControllerBase
+    {
+
+        private readonly ILogger<SecretSantaController> _logger;
+        private readonly ISecretSantaGenerator SecretSantaGenerator;
+
+        public SecretSantaController(ILogger<SecretSantaController> logger, ISecretSantaGenerator secretSantaGenerator)
+        {
+            this._logger = logger;
+            this.SecretSantaGenerator = secretSantaGenerator;
+        }
+    }
+}
