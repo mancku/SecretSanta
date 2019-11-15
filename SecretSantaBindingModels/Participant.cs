1a05d74fe3ea8ccd74d8a56b8aa4ce503f8fd2df
Adria Lopez <adria.lopez@appsnfriends.com>
2019-11-15 15:55:47 +0100

    Added binding models project

---
+++ SecretSantaBindingModels/Participant.cs
@@ -0,0 +1,74 @@
+﻿using SecretSantaBindingModels.ValidationAttributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SecretSantaBindingModels
+{
+    public class Participant : IEquatable<Participant>
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+
+        [MinLength(2, ErrorMessage = "The Last Name is too short")]
+        public string LastName { get; set; }
+
+        [ValueIsEmail]
+        [OtherPropertyIsInformedIfThisIsNot("PhoneNumber",
+             ErrorMessage = "The 'PhoneNumber' field must contain information when the field 'EMailAddress' is empty.")]
+        public string EMailAddress { get; set; }
+        [MinLength(9, ErrorMessage = "The phone number is too short")]
+        public string PhoneNumber { get; set; }
+
+        public bool Equals(Participant other)
+        {
+            if (ReferenceEquals(null, other))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return string.Equals(this.FirstName, other.FirstName)
+                   && string.Equals(this.LastName, other.LastName)
+                   && string.Equals(this.EMailAddress, other.EMailAddress)
+                   && string.Equals(this.PhoneNumber, other.PhoneNumber);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj.GetType() == this.GetType()
+                   && this.Equals((Participant)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = (this.FirstName != null ? this.FirstName.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (this.LastName != null ? this.LastName.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (this.EMailAddress != null ? this.EMailAddress.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (this.PhoneNumber != null ? this.PhoneNumber.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+
+        public override string ToString()
+        {
+            return
+                $"Full name: {this.FirstName} {this.LastName}, Email: {this.EMailAddress}, Phone: {this.PhoneNumber}";
+        }
+    }
+}
---
+++ SecretSantaBindingModels/ValidationAttributes/ValueIsEmailAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace SecretSantaBindingModels.ValidationAttributes
+{
+    public class ValueIsEmailAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != null)
+            {
+                if (string.IsNullOrWhiteSpace(value.ToString()))
+                {
+                    return ValidationResult.Success;
+                }
+
+                var isEmail = Regex.IsMatch(value.ToString(),
+                    @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
+                    RegexOptions.IgnoreCase);
+
+                return isEmail
+                    ? ValidationResult.Success
+                    : new ValidationResult(this.ErrorMessage);
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
