b56df25bc4c7c09297b634e94b9cffc047c5327f
Adria Lopez <adria.lopez@appsnfriends.com>
2019-11-18 17:30:46 +0100

    Created interface with the minimum participant information

---
+++ SecretSanta/IParticipant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretSanta
+{
+    public interface IParticipant
+    {
+        string FirstName { get; set; }
+        string Email { get; set; }
+    }
+}
